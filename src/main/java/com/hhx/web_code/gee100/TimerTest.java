0daaf5e276eabca426735366d9687436d3b48d78
hhx <>
2022-06-13 10:41:05 +0800

    每秒执行一次脚本

---
+++ src/main/java/com/hhx/web_code/gee100/TimerTest.java
@@ -0,0 +1,20 @@
+package com.hhx.web_code.gee100;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+@Slf4j
+public class TimerTest {
+
+    private AtomicInteger atomicInteger = new AtomicInteger();
+
+
+    private void perSecend(){
+          //每秒执行一次
+          Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(()->{
+              log.info("DB QPS:{}",atomicInteger.getAndSet(0));
+          },0,1, TimeUnit.SECONDS);
+    }
+}
