bb40de0b106c5023e1615c7714aef7419dfdfb9d
hhx <>
2022-06-15 08:56:34 +0800

    @Component和 @Bean注解

--- src/main/java/com/hhx/web_code/springlesson/BeanAutowiredTest.java
+++ src/main/java/com/hhx/web_code/springlesson/BeanAutowiredTest.java
@@ -21,6 +21,14 @@ public class BeanAutowiredTest {
      * 而 CassandraDataService 的 Bean 名称则变成了首字母小写（cassandraDataService）。
      */
 
+    /**
+     * @Component 表明一个类会作为组件类，并告诉Spring要为这个类创建bean，通常是通过类路径扫描来自动侦测
+     *  以及自动装配到Sprinig容器中。
+     * @Bean 注解告诉Spring这个方法将会返回一个对象，这个对象要注册为Spring应用上下文中的bean
+     *  通常是我们子啊标有该注解的方法中定义产生这个bean的逻辑
+     *  当应用第三方库中的类需要装配到Spring中时，智能通过@Bean来实现
+     */
+
 
 
 
