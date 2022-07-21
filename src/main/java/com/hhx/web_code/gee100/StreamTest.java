380fb1f32abcabe3e99f79d570258b589b26f427
hhx <>
2022-07-21 17:30:46 +0800

    flatMap

--- src/main/java/com/hhx/web_code/gee100/StreamTest.java
+++ src/main/java/com/hhx/web_code/gee100/StreamTest.java
@@ -3,9 +3,7 @@ package com.hhx.web_code.gee100;
 import com.hhx.web_code.lambada.Student;
 import org.apache.tomcat.util.net.WriteBuffer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -24,7 +22,31 @@ public class StreamTest {
         list.parallelStream().filter((Student s) -> s.getHeight()>160).
                 collect(Collectors.groupingBy(Student::getHeight));
 
+    }
 
+    private void getConsumableMediaTypes(){
+        List<String> teamIndia = Arrays.asList("Virat", "Dhoni", "Jadeja");
+        List<String> teamAustralia = Arrays.asList("Warner", "Watson", "Smith");
+        List<String> teamEngland = Arrays.asList("Alex", "Bell", "Broad");
+        List<String> teamNewZeland = Arrays.asList("Kane", "Nathan", "Vettori");
+        List<String> teamSouthAfrica = Arrays.asList("AB", "Amla", "Faf");
+        List<String> teamWestIndies = Arrays.asList("Sammy", "Gayle", "Narine");
+        List<String> teamSriLanka = Arrays.asList("Mahela", "Sanga", "Dilshan");
+        List<String> teamPakistan = Arrays.asList("Misbah", "Afridi", "Shehzad");
+
+        List<List<String>> playersInWorldCup2016 = new ArrayList<>();
+        playersInWorldCup2016.add(teamIndia);
+        playersInWorldCup2016.add(teamAustralia);
+        playersInWorldCup2016.add(teamEngland);
+        playersInWorldCup2016.add(teamNewZeland);
+        playersInWorldCup2016.add(teamSouthAfrica);
+        playersInWorldCup2016.add(teamWestIndies);
+        playersInWorldCup2016.add(teamSriLanka);
+        playersInWorldCup2016.add(teamPakistan);
+
+        List<String> flatMapList = playersInWorldCup2016.stream()
+                .flatMap(pList -> pList.stream())
+                .collect(Collectors.toList());
 
     }
 }
