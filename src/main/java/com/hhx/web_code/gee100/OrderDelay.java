40f64470b17fa434eab924b5aa1e70f53cb63700
hhx <>
2022-07-22 17:03:08 +0800

    Delay

--- src/main/java/com/hhx/web_code/gee100/OrderDelay.java
+++ src/main/java/com/hhx/web_code/gee100/OrderDelay.java
@@ -1,5 +1,8 @@
 package com.hhx.web_code.gee100;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
@@ -36,5 +39,26 @@ public class OrderDelay implements Delayed {
         return (d == 0) ? 0 : ((d < 0) ? -1 : 1);
     }
 
+    void print() {
+        System.out.println("OrderDelay.print");
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+
+        DelayQueue<OrderDelay> queue = new DelayQueue<OrderDelay>();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < list.size(); i++) {
+            queue.put(new OrderDelay(list.get(i), TimeUnit.NANOSECONDS.convert(10, TimeUnit.SECONDS)));
+            try {
+                queue.take().print();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
 
 }
